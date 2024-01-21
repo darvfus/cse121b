5bdc5416dfcab746dfd932a197e95db50887300b
darvfus <romerondan123@gmail.com>
2024-01-20 20:07:03 -0600

    "xhange for week2.1"

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -25,32 +25,24 @@ document
 
 /* ARRAYS */
 
-// Step 1: Declare and instantiate an array variable to hold your favorite foods
 const foodFavs = ["Tacos", "Tamales", "Picaditas", "pepperoni pizza", "Pozole"];
 
-// Step 2: Update the HTML element with the ID "food" to display the favorite foods array
 document.querySelector("#food").textContent = foodFavs.join(",");
 
-// Step 3: Declare and instantiate a variable to hold another favorite food
 const moreFoodFavs1 = "Chagalapolly water";
 
-// Step 4: Add the variable holding another favorite food to the favorite food array
 
 
-// Step 5: Update the HTML element with the ID "food" to display the modified array with line breaks
 document.querySelector("#food").innerHTML = foodFavs.join(",");
 foodFavs.push(moreFoodFavs1);
-// Step 6: Remove the first element in the favorite foods array
+
 const removedFirstFood = foodFavs.shift();
 
-// Step 7: Update the HTML element with the ID "food" to display the modified array with the removed first element and line breaks
 document.querySelector("#food").innerHTML += "<br>" + removedFirstFood + "," + foodFavs.join(",");
 
-// Step 8: Remove the last element in the favorite foods array
 const removedLastFood = foodFavs.pop();
 
 document.querySelector("#food").innerHTML += "<br>"+ foodFavs+","+ removedLastFood;
 
 
-// Step 9: Update the HTML element with the ID "food" to display the modified array with the removed last element and line breaks
 document.querySelector("#food").innerHTML += "<br>"+ foodFavs.join(",");
